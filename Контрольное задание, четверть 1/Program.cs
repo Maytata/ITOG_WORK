72e12f47e725bde86c44273742bec4d337bc218a
PavelVovk <106928926+Maytata@users.noreply.github.com>
2022-08-20 22:54:57 +0300

    Программа написана

---
+++ Контрольное задание, четверть 1/Program.cs
@@ -0,0 +1,48 @@
+﻿Zadacha();
+       void Zadacha()
+       {
+        Random rand = new Random();
+        int numb = rand.Next(3,8);
+         string[] array = new string[numb];
+
+
+        Console.WriteLine("Заполните массив строками:");
+        FillArray(array);
+        Console.WriteLine("Первичный массив:");
+        PrintArray(array);
+        Console.WriteLine();
+        Console.WriteLine("Вторичный массив:");
+        SortArray(array);
+
+       }
+      
+
+       string[] FillArray(string[] arr)
+       {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Console.ReadLine();
+            }
+            return arr;
+       }
+        
+       string[] PrintArray(string[] arr)
+       {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + "\t");
+            }
+            return arr;
+       }
+
+       void SortArray(string[] arr)
+       {
+        for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].Length <= 3)
+                { 
+                    Console.Write(arr[i] + "\t");
+                }
+            }
+          Console.WriteLine();
+       }
